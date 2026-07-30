10078411b4c2ce2707942b6971e2fa4de1cda2cd
Rony Shmidov <70490582+Dovshmi@users.noreply.github.com>
2026-07-30 18:20:46 +0300

    Load timer preset selector enhancement on test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './test-heading-cleanup.css';
+import './timer-preset-select.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode><App /></StrictMode>,
